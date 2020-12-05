9313ff57bd274ec58d4819b48de803c0bed68c33
Xenvex <matthewbreyes@gmail.com>
2020-12-04 20:47:30 -0800

    ADFLSDJGNHSDJLKGd

--- Assets/Debbie/Scripts/Player.cs
+++ Assets/Debbie/Scripts/Player.cs
@@ -11,12 +11,17 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
     private Animator anim;
 
+    //Boundaries
+    public float zMaxBoundary, zMinBoundary;
+
     //PickUpVariables
     public int health = 100;
     public int Grenade = 0;
     public bool Blaster = false;
 
     public HealthBar healthbar;
+
+    public bool MoveBack = true;
 
     void Start()
     {
@@ -48,8 +53,12 @@ public class Player : MonoBehaviour
         }
 
         // Moves player back and front by axis and transform
-        float zAxis = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(0, 0, zAxis) * moveSpeed * Time.deltaTime);
+        if(MoveBack == true)
+        {
+            float zAxis = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(0, 0, zAxis) * moveSpeed * Time.deltaTime);
+        }
+        
 
         // When 'Space' is pressed, player shoots
         if (Input.GetKeyDown(KeyCode.Space))
@@ -72,6 +81,25 @@ public class Player : MonoBehaviour
         {
             Kick();
         }
+
+
+        //Moves the player within the boundaries
+        
+        if (transform.position.z < zMinBoundary)
+        {
+            MoveBack = false;//moveSpeed = 0;
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMinBoundary + 0.1f);
+        }
+        else if (transform.position.z > zMaxBoundary)
+        {
+            MoveBack = false;//moveSpeed = 0;
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMaxBoundary - 0.1f);
+        }
+        else
+        {
+            MoveBack = true;
+        }
+        
     }
 
     private void OnCollisionEnter(Collision other)
@@ -79,13 +107,14 @@ public class Player : MonoBehaviour
         if(other.gameObject.tag == "Enemy")
         {
             Debug.Log("Get Rekt Kid");
-            Enemy currentEnemy = other.gameObject.GetComponent<Enemy>(); //Long ago in a land far away, there once was a 
+            Enemy currentEnemy = other.gameObject.GetComponent<Enemy>(); //Long ago in a land far away, there once was a knight who went to slay a dragon. Along their journey they met a scarecrow 
             currentEnemy.health -= 30;
             Debug.Log("Enemy Health from Player Perspective" + currentEnemy.health);
             //currentEnemy.TakeDamage(30);
             //Destroy(other);
         }
     }
+
 
     void Punch()
     {
